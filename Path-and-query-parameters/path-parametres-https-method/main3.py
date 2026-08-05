cb3423e0aaffcc638fa865a154f63378a801a803
knking <knr020498@gmail.com>
2026-08-05 11:59:29 +0530

    path parameter with predefined value

---
+++ Path-and-query-parameters/path-parametres-https-method/main3.py
@@ -0,0 +1,36 @@
+
+from fastapi import FastAPI
+from enum import Enum
+app = FastAPI()
+
+class allowedcategory(str,Enum):
+    books="books"
+    clothing="jeans"
+    electronics="mobile"
+
+
+
+@app.get("/")
+async def prodcut():
+    return "hello from fastapi"
+
+
+# @app.get("/product/{category}")
+# async def prodcut(category:allowedcategory):
+#     return {"responce":"product fetched", "category is ": category}
+
+##working with python enumeration
+
+@app.get("/product/{category}")
+async def prodcut(category:allowedcategory):
+    if category == allowedcategory.books:
+        return {"responce":"Books are awsome"}
+    elif category.value=="jeans":
+        return {category.value: "Clothing is cool"}
+    elif category == allowedcategory.electronics.value:
+        return {"responce":"mobile are awsome"}
+    else:
+        return {"responce": " Unknown category"}
+
+    
+
